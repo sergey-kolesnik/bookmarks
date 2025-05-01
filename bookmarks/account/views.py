350b41bbdd104e1fa8e5ac075400d9f2bd4fe89d
Колесник Сергей <serg@astra-live>
2025-05-01 18:04:43 +0300

    update views.py create forms.py

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import authenticate, login
+from django.http import (
+    HttpResponse, 
+    HttpRequest,
+)
 from django.shortcuts import render
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request: HttpRequest):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username = cd["username"],
+                password = cd["password"],
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated sus=ccessfully")
+                else:
+                    return HttpResponse("Disabled account")
+            else:
+                return HttpResponse("invalid login")
+        else:
+            form = LoginForm()
+        return render(
+            request,
+            "account/lohin.html",
+            {
+                "form": form
+            }
+        )
